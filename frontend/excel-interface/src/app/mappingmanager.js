035263aa5641297dedeaa2cb830f1e8ccfb8f0f9
dinoplane <isacsac2017@gmail.com>
2023-11-12 21:39:01 -0800

    initial mapping manager

--- frontend/excel-interface/src/app/mappingmanager.js
+++ frontend/excel-interface/src/app/mappingmanager.js
@@ -1,19 +1,36 @@
 export default class MappingManager {
+  constructor() {
+    this.mappingDbUrl = "0.0.0.0";
+    this.fusekiDbUrl = "127.0.0.1";
+  }
 
-    constructor(){
-        this.mappingDbUrl = ""
-        this.fusekiDbUrl = ""
-    }
+  async createMapping(entry) {
+    const response = await fetch(this.mappingDbUrl, {
+      method: "POST",
+      body: JSON.stringify({type: "create", fusekiUrl: this.fusekiDbUrl, mapping: entry})
+    });
+    const success = await response.json();
+    console.log(mapping);
+    return success;
+  }
 
-    createMapping(entry){
-        console.log(entry);
-    }
+  async deleteMapping(entry) {
+    const response = await fetch(this.mappingDbUrl, {
+      method: "POST",
+      body: JSON.stringify({type: "delete", fusekiUrl: this.fusekiDbUrl, mapping: entry})
+    });
+    const success = await response.json();
+    console.log(mapping);
+    return success;
+  }
 
-    deleteMapping(entry){
-        console.log(entry);
-    }
-
-    requestMapping(begin, end){
-        console.log(begin, end);
-    }
+  async requestMapping(b, e) {
+    const response = await fetch(this.mappingDbUrl, {
+      method: "GET",
+      body: JSON.stringify({type: "request", fusekiUrl: this.fusekiDbUrl, begin: b, end: e})
+    });
+    const mappings = await response.json();
+    console.log(mapping);
+    return mappings;
+  }
 }
