b77c55d57774ea140e428fc20f6e5ec8dd623371
steph <70790806+stephucla@users.noreply.github.com>
2023-12-02 22:59:10 -0800

    start fuseki test

---
+++ backend/tests/fuseki_manager_tests.py
@@ -0,0 +1,24 @@
+#get our app from backend
+from app.main import app
+import os
+from app.schema.mapping_schema import MappingEntry
+
+#use pytest
+import pytest
+#use TestClient
+from fastapi.testclient import TestClient
+
+@pytest.fixture
+def api_routes():
+    return {"fuseki_interface":"/excel-interface/operations"}
+
+@pytest.fixture
+def fuseki_connec():
+    return {"url_1":"http://localhost:3030/db/"}
+
+#tests apply rn when Fuseki server running on "http://localhost:3030/db/"
+def test_fuseki_connection(api_routes, fuseki_connec):
+    with TestClient(app) as tester:
+        resp = tester.post(api_routes["fuseki_interface"]+"/check-connection", json={"fuseki_url": fuseki_connec["url_1"]})
+        assert resp.status_code == 200
+    return 
